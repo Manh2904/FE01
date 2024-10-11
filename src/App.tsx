f69f2ae9278a62cd2830e5350bb31be4f9741348
Quynh12345678 <phunhmanhquynh@gmail.com>
2024-10-11 22:54:01 +0700

    gg client

---
+++ src/App.tsx
@@ -0,0 +1,99 @@
+import { useRoutes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './compotion/home';
+import 'react-toastify/dist/ReactToastify.css';
+import ClientLayout from './layouts/clientLayout';
+import Login from './compotion/login';
+import Products from './compotion/products';
+import Register from './compotion/register';
+import Pay from './pages/Website/home/pay';
+import Bill from './pages/Website/home/bill';
+import DetailBill from './pages/Website/home/detailBill';
+import SingleProduct from './compotion/singleProduct';
+
+import NotFound from './compotion/notFound';
+import Abc from './compotion/Giohang';
+
+
+
+//   return (
+//     <>
+//     <Routes>
+//       {/* <Route path='/' Component={Home} /> */}
+//       <Route path='/product' Component={Products} />
+//       <Route path='/login' Component={Login} />
+//       <Route path='/register' Component={Register} />
+//       <Route path='/single/:id' element={<SingleProduct product = {listProduct} setProduct ={setlistProduct}/>}/>
+//       {/* <Route path='/s' Component={Load} /> */}
+//       {/* <Route path='/admin' Component={HomeAdmin}/>
+//       <Route path='/admin/list'  element={<Listproduct product = {listProduct} setProduct ={setlistProduct}/>}/>
+//       <Route path='/admin/add' Component={AddProduct}/>
+//       <Route path='/admin/update/:id' Component={UpdateProduct}/> */}
+//       <Route path='*' element={<NotFound />} /> 
+//     </Routes>
+//     </>
+//   )
+// }
+
+// export default App
+
+const routerConfig = [
+  {
+    path: "/",
+    element: <ClientLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/product",
+        element: <Products />,
+      },
+  
+      {
+        path: "/pay",
+        element: <Pay />,
+      },
+      {
+        path: "/bill",
+        element: <Bill />,
+      },
+      {
+        path: "/detailbill",
+        element: <DetailBill />,
+      },
+      {
+        path: "/single",
+        element: <SingleProduct />,
+      },
+      {
+        path: "/*",
+        element: <NotFound />,
+      },
+
+    ],
+  },
+  {
+    path: "/abc",
+    element: <Abc/>
+  }
+  
+]
+
+function App() {
+  const routes = useRoutes(routerConfig);
+  return (
+    <>{routes}</>
+  );
+}
+
+export default App;
---
+++ src/compotion/load.tsx
@@ -0,0 +1,33 @@
+
+import '/src/assets/css/style.css'
+
+const Load = () => {
+  
+ 
+    return (
+       <div className={'container'}>
+          <div className={'row'}>
+            <div className="col-6">
+            <div className={'main-img'}>
+                <img src="" width={500} height={450} alt="" />
+             </div>
+             <div className={'list-smail-img'}>
+                <div className={'smail-img'}>
+                   <img src="src/images/image 57.png" width={120} alt="" />
+                </div>
+                <div className={'smail-img'}>
+                   <img src="src/images/image 58.png" width={120} alt=""  />
+                </div>
+                <div className={'smail-img'}>
+                   <img src="src/images/image 61.png" width={120} alt=""/>
+                </div>
+                <div className={'smail-img'}>
+                   <img src="src/images/image 63.png" width={120} alt="" />
+                </div>
+             </div>
+            </div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+ ) }
+export default Load
